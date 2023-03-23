f7264d5c78c7d2bbc73b7202078f513a04849caf
timohuovinen <timoh89@gmail.com>
2023-03-23 11:04:45 +0100

    use v0.1.1 and remove camel case conversion

--- COMPILE-PROTOS.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-TMP_GOBIN=$(mktemp -d -t gnostic-grpc.XXXXXXXXXX)
-
-GOBIN=${TMP_GOBIN} go install ./search
-GOBIN=${TMP_GOBIN} go install -mod=mod github.com/golang/protobuf/protoc-gen-go@v1.5.2
-PATH="$TMP_GOBIN:$PATH" protoc --go_out=incompatibility/ ./incompatibility/incompatibility-report.proto
-
-rm -rf "${TMP_GOBIN}"
--- plugin-creation.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gopath=$(go env GOPATH)
-./COMPILE-PROTOS.sh
-cd plugin
-go build -o ${gopath}/bin/gnostic-grpc 
